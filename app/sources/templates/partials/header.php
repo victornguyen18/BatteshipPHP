cdab11aa7e81ad9ca07b4440f523e06b1b69f9e8
nnta.zip <32012708+nntadotzip@users.noreply.github.com>
2018-05-07 18:29:24 +0700

    - Add UI

--- app/sources/templates/partials/header.php
+++ app/sources/templates/partials/header.php
@@ -15,28 +15,70 @@
 </head>
 <body>
 <div class="wrapper">
-    <header>
-        <nav id="myNavBar" class="navbar" role="navigation">
-            <div class="container-fluid">
-                <div class="navbar-header">
-                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavBar-collapse">
-                        <span class="sr-only">Toggle navigation</span>
-                        <span class="icon-bar">Link 1</span>
-                        <span class="icon-bar">Link 2</span>
-                        <span class="icon-bar">Link 3</span>
-                    </button>
-                </div>
-                <div class="collapse navbar-collapse" id="myNavBar-collapse">
-                    <ul class="nav navbar-nav">
-                        <li class="active"></li>
-                        <li></li>
-                        <li style="margin-top: 20px"><a class="logo-border" href="#">
-                                <img class="logo" src="<?php echo URL; ?>img/ship/logo.png" alt="Battleship Online">
-                            </a></li>
-                        <li></li>
-                        <li></li>
-                    </ul>
+<!--    <header>-->
+<!--        <nav id="myNavBar" class="navbar" role="navigation">-->
+<!--            <div class="container-fluid">-->
+<!--                <div class="navbar-header">-->
+<!--                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavBar-collapse">-->
+<!--                        <span class="sr-only">Toggle navigation</span>-->
+<!--                        <span class="icon-bar">Link 1</span>-->
+<!--                        <span class="icon-bar">Link 2</span>-->
+<!--                        <span class="icon-bar">Link 3</span>-->
+<!--                    </button>-->
+<!--                </div>-->
+<!--                <div class="collapse navbar-collapse" id="myNavBar-collapse">-->
+<!--                    <ul class="nav navbar-nav">-->
+<!--                        <li class="active"></li>-->
+<!--                        <li></li>-->
+<!--                        <li style="margin-top: 20px"><a class="logo-border" href="#">-->
+<!--                                <img class="logo" src="--><?php //echo URL; ?><!--img/ship/logo.png" alt="Battleship Online">-->
+<!--                            </a></li>-->
+<!--                        <li></li>-->
+<!--                        <li></li>-->
+<!--                    </ul>-->
+<!--                </div>-->
+<!--            </div>-->
+<!--        </nav>-->
+<!--    </header>-->
+    <section id="pheader">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <div id="ptopnav">
+                        <div class="pull-right login-box">
+                            <a href="/dang-ky.html">SIGN UP</a>
+                            <a href="/dang-nhap.html">SIGN IN</a>
+                            <a href="/lang/vi" class="lang"><img src="<?php echo URL; ?>img/vi.png"/></a>
+                            <a href="/lang/en" class="lang"><img src="<?php echo URL; ?>img/en.png"/></a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </nav>
-    </header>
+            <nav id="pmainmenu">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="left-menu">
+                            <ul>
+                                <li class="active"><a href="/">GO BATTLE</a></li>
+                                <li><a href="#">HOME</a></li>
+                                <li><a href="#">How to play</a></li>
+                                <li><a href="#">More games</a></li>
+                            </ul>
+                        </div>
+                        <div class="center-menu">
+                            <a href="/"><img src="<?php echo URL; ?>img/ship/logo.png"
+                                             style="width: 250px; height: 200px; -webkit-transform: scaleX(-1); transform: scaleX(-1); z-index: 1;"/></a>
+                        </div>
+                        <div class="right-menu">
+                            <ul>
+                                <li><a href="#">SHOP</a></li>
+                                <li><a href="#">BATTLE EVENTS</a></li>
+                                <li><a href="/tuyen-dung.html">Help</a></li>
+                                <li><a href="/lien-he">ABOUT US</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </div>
+    </section>
--- app/sources/views/play/index.php
+++ app/sources/views/play/index.php
@@ -1,4 +1,55 @@
+<section id="pslideshow" class="carousel slide" data-ride="carousel">
+    <!-- Indicators -->
+    <ol class="carousel-indicators">
+        <li data-target="#pslideshow" data-slide-to="0" class="active"></li>
+        <li data-target="#pslideshow" data-slide-to="1"></li>
+        <li data-target="#pslideshow" data-slide-to="2"></li>
+    </ol>
 
+    <!-- Wrapper for slides -->
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img src="<?php echo URL; ?>img/slideshow_1.jpg" height="445px">
+            <div class="carousel-caption">
+                <div class="pizza-logo">
+                    <p><img src="<?php echo URL; ?>img/bomb.png"/></p>
+                    <p style="font-size: 30px; margin: 20px;">NEW FEATURE</p>
+                    <a href="" class="btn btn-danger">PLAY NOW</a>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="<?php echo URL; ?>img/slideshow_2.jpg" height="445px">
+            <div class="carousel-caption">
+                <div class="pizza-logo">
+                    <p><img src="<?php echo URL; ?>img/radar.png"/></p>
+                    <p style="font-size: 30px; margin: 20px;">NEW FEATURE</p>
+                    <a href="" class="btn btn-danger">BUY NOW</a>
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img src="<?php echo URL; ?>img/slideshow_3.jpg" height="445px">
+            <div class="carousel-caption">
+                <div class="pizza-logo">
+                    <p><img src="<?php echo URL; ?>img/bomb.png"/></p>
+                    <p style="font-size: 30px; margin: 20px;">NEW FEATURE</p>
+                    <a href="" class="btn btn-danger">TRY IT OUT</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Controls -->
+    <a class="left carousel-control" href="#pslideshow" role="button" data-slide="prev">
+        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+        <span class="sr-only">Previous</span>
+    </a>
+    <a class="right carousel-control" href="#pslideshow" role="button" data-slide="next">
+        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+        <span class="sr-only">Next</span>
+    </a>
+</section>
 <div class="container">
     <div class="title">PLACE YOUR SHIPS</div>
     <div class="frame" id="frame">
