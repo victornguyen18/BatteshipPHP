74430d97223b5b4f66fd9792c28db838e9826c09
Thắng Nguyễn <thang181997@gmail.com>
2018-08-20 19:37:45 +0700

    Tesst

--- app/config/paths.php
+++ app/config/paths.php
@@ -6,7 +6,7 @@
  * Time: 03:41 PM
  */ 
 define('ADMIN_THEME','http://local.deal.vn/AdminLTE/');
-define('SITE_THEME','site');
+define('SITE_THEME','sites');
 define("URL","http://".$_SERVER['HTTP_HOST']."/");
 define("STATIC_URL","http://".$_SERVER['HTTP_HOST']."/");
 define("URL_SITE","http://".$_SERVER['HTTP_HOST']."/");
